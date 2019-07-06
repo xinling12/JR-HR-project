aeea9900a83cfb0916e21b3e4908fd4284626a2f
sarah <lwz6698@gmail.com>
2019-07-06 22:22:18 +1000

    carousel

--- hr-app/src/components/Home.jsx
+++ hr-app/src/components/Home.jsx
@@ -1,50 +1,86 @@
 import React from 'react';
-// import Carousel from 'react-bootstrap/Carousel';
-import { Container } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import styled from 'styled-components';
+
+
+const Styles = styled.div`
+.bg-box {
+    margin-bottom: 200px;
+}
+.text {
+    margin-top: 40px;
+    color: lightgrey;
+}
+.heading-2 {
+    color: navy; 
+    margin-left: 600px;
+}
+.text-2 {
+    margin-top: 40px; 
+    margin-left: 590px;
+}
+.bg-box-3 {
+    margin-bottom: 280px;
+}
+.heading-3 {
+    margin-bottom: 50px;
+}
+
+`
 
 export const Home = () => (
-    // <Carousel>
-    //     <Carousel.Item>
-    //         <img
-    //             className="d-block w-100"
-    //             src="../assets/bgimg.jpg"
-    //             alt="First slide"
-    //         />
-    //         <Carousel.Caption>
-    //             <h3>First slide label</h3>
-    //             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    //         </Carousel.Caption>
-    //     </Carousel.Item>
-    //     <Carousel.Item>
-    //         <img
-    //             className="d-block w-100"
-    //             src="../assets/bgimg.jpg"
-    //             alt="Third slide"
-    //         />
-
-    //         <Carousel.Caption>
-    //             <h3>Second slide label</h3>
-    //             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    //         </Carousel.Caption>
-    //     </Carousel.Item>
-    //     <Carousel.Item>
-    //         <img
-    //             className="d-block w-100"
-    //             src="../assets/bgimg.jpg"
-    //             alt="Third slide"
-    //         />
-
-    //         <Carousel.Caption>
-    //             <h3>Third slide label</h3>
-    //             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    //         </Carousel.Caption>
-    //     </Carousel.Item>
-    // </Carousel>
-
-    <Container>
-        <h2>Welcome to home page</h2>
-        <p>HR management system for company</p>
-    </Container>
+    <Styles>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={580}
+                    src={require('../assets/bg3.jpg')}
+                    alt="First slide"
+                />
+                <Carousel.Item></Carousel.Item>
+                <Carousel.Caption className="bg-box">
+                    <h1><strong>Upcoming Events</strong></h1>
+                    <div className="text">
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={580}
+                    src={require('../assets/bg2.jpeg')}
+                    alt="Second slide"
+                />
+                <Carousel.Item></Carousel.Item>
+                <Carousel.Caption className="bg-box">
+                    <h1 className="heading-2"><strong>HR News</strong></h1>
+                    <div className="text-2">
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    height={580}
+                    src={require('../assets/bg-1.jpg')}
+                    alt="Third slide"
+                />
+                <Carousel.Item></Carousel.Item>
+                <Carousel.Caption className="bg-box border">
+                    <h1><strong>Weekly Report</strong></h1>
+                    <div className="text">
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    </Styles>
 )
 
 
--- hr-app/src/components/NavBar.jsx
+++ hr-app/src/components/NavBar.jsx
@@ -3,8 +3,6 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-// import { faSearch } from '@fortawesome/fontawesome-free-solid';
 import styled from 'styled-components';
 
 
@@ -42,7 +40,6 @@ const Styles = styled.div`
 
 export const NavBar = () => (
     <Styles>
-        {/* <Navbar className="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar "> */}
         <Navbar bg="primary" variant="dark">
             <Navbar.Brand href="/">Home Page</Navbar.Brand>
             <Nav className="mr-auto">
@@ -54,56 +51,6 @@ export const NavBar = () => (
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                 <Button variant="outline-light">Search</Button>
             </Form>
-
-
-            {/* <Navbar.Brand href="/">Home Page</Navbar.Brand>
-            <Nav.Item><Nav.Link href="/company">Company Profile</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/employeelist">Employee List</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="/payroll">Payroll Management</Nav.Link></Nav.Item> */}
-
         </Navbar>
-
-
-
-
-        {/* <Nav className="ml-auto">
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Search..."
-                        aria-label="Search..."
-                        aria-describedby="basic-addon2"
-                    />
-                    <InputGroup.Append>
-                        <Button className="search-btn" variant="outline-secondary">
-                            <FontAwesomeIcon icon="search" />
-                        </Button>
-                    </InputGroup.Append>
-                </InputGroup> */}
-
-        {/* <Nav.input className="search-input" type="text" value placeholder="Search..."></Nav.input>
-                <Nav.button>className="search-btn"><i className="fa fa-search"></i></Nav.button> */}
-
-
-
-
-        {/* <ul className="navbar-nav">
-                <li className="nav-item nav-link nav-brand active">
-                    <Nav.Link href="/home">Home Page</Nav.Link>
-                </li>
-                <li className="nav-item nav-link">
-                    <Nav.Link href="/company">Company Profile</Nav.Link>
-                </li>
-                <li className="nav-item nav-link">
-                    <Nav.Link href="/employeelist">Employee List</Nav.Link>
-                </li>
-                <li className="nav-item nav-link">
-                    <Nav.Link href="/payroll">Payroll Management</Nav.Link>
-                </li>
-            </ul>
-            <ul className="navbar-nav felx-row ml-md-auto d-md-flex">
-                <input className="search-input" type="text" value placeholder="Search..." />
-                <button className="search-btn"><i className="fa fa-search"></i></button>
-            </ul> */}
-
     </Styles >
 )
