5d1e7ad5b00f37e61fc283ed5ff813ecbc332385
xinling12 <45545665+xinling12@users.noreply.github.com>
2019-07-21 08:06:19 +1000

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps:{
+        name:"jr-hr-api",
+        script:"./src/index.js",
+        instance:'max',
+        env_production:{
+            NODE_ENV:'production'
+        }
+    }
+}
